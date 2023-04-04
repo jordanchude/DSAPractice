ef78721987732ca18fb26be60a20b3765707bdc8
Jordan Williams <Jordan@Emilys-MacBook-Air.local>
2023-04-03 20:28:29 -0400

    Refactor merge function to mergeIntervals and add test cases in test.js file

--- array-and-string-manipulation/merge-intervals/solution.js
+++ array-and-string-manipulation/merge-intervals/solution.js
@@ -14,10 +14,12 @@
 //  * @return {number[][]}
 //  */
 
-var merge = function(intervals) {
+var mergeIntervals = function(intervals) {
     // input: an array of arrays
     // output: an array of arrays
     // notes: double loop O(n2)
+
+    if (intervals.length === 0) return intervals;
 
     // pseudocode
     // create output array
@@ -43,8 +45,43 @@ var merge = function(intervals) {
       }
 
     }
-    
+
     return mergedIntervals;
 };
 
-console.log(merge([[1,3],[2,6],[8,10],[15,18]]))
+console.log(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]))
+
+// TESTS
+
+// Test cases for the mergeIntervals function
+// Using console.log and JSON.stringify to compare arrays
+
+// Test case 1: intervals = [[1,3], [2,6], [8,10], [15,18]]
+let intervals1 = [[1,3], [2,6], [8,10], [15,18]];
+let expected1 = [[1,6], [8,10], [15,18]];
+let actual1 = mergeIntervals(intervals1);
+console.log(JSON.stringify(actual1) === JSON.stringify(expected1)); // true
+
+// Test case 2: intervals = [[1,4], [4,5]]
+let intervals2 = [[1,4], [4,5]];
+let expected2 = [[1,5]];
+let actual2 = mergeIntervals(intervals2);
+console.log(JSON.stringify(actual2) === JSON.stringify(expected2)); // true
+
+// Test case 3: intervals = [[1,2], [3,4], [5,6]]
+let intervals3 = [[1,2], [3,4], [5,6]];
+let expected3 = [[1,2], [3,4], [5,6]];
+let actual3 = mergeIntervals(intervals3);
+console.log(JSON.stringify(actual3) === JSON.stringify(expected3)); // true
+
+// Test case 4: intervals = [[1,10], [2,3], [4,5], [6,7], [8,9]]
+let intervals4 = [[1,10], [2,3], [4,5], [6,7], [8,9]];
+let expected4 = [[1,10]];
+let actual4 = mergeIntervals(intervals4);
+console.log(JSON.stringify(actual4) === JSON.stringify(expected4)); // true
+
+// Test case 5: intervals = []
+let intervals5 = [];
+let expected5 = [];
+let actual5 = mergeIntervals(intervals5);
+console.log(JSON.stringify(actual5) === JSON.stringify(expected5)); // true
--- array-and-string-manipulation/merge-intervals/test.js
+++ array-and-string-manipulation/merge-intervals/test.js
@@ -1,1 +1,32 @@
- 
+ // Test cases for the mergeIntervals function
+// Using console.log and JSON.stringify to compare arrays
+
+// Test case 1: intervals = [[1,3], [2,6], [8,10], [15,18]]
+let intervals1 = [[1,3], [2,6], [8,10], [15,18]];
+let expected1 = [[1,6], [8,10], [15,18]];
+let actual1 = mergeIntervals(intervals1);
+console.log(JSON.stringify(actual1) === JSON.stringify(expected1)); // true
+
+// Test case 2: intervals = [[1,4], [4,5]]
+let intervals2 = [[1,4], [4,5]];
+let expected2 = [[1,5]];
+let actual2 = mergeIntervals(intervals2);
+console.log(JSON.stringify(actual2) === JSON.stringify(expected2)); // true
+
+// Test case 3: intervals = [[1,2], [3,4], [5,6]]
+let intervals3 = [[1,2], [3,4], [5,6]];
+let expected3 = [[1,2], [3,4], [5,6]];
+let actual3 = mergeIntervals(intervals3);
+console.log(JSON.stringify(actual3) === JSON.stringify(expected3)); // true
+
+// Test case 4: intervals = [[1,10], [2,3], [4,5], [6,7], [8,9]]
+let intervals4 = [[1,10], [2,3], [4,5], [6,7], [8,9]];
+let expected4 = [[1,10]];
+let actual4 = mergeIntervals(intervals4);
+console.log(JSON.stringify(actual4) === JSON.stringify(expected4)); // true
+
+// Test case 5: intervals = []
+let intervals5 = [];
+let expected5 = [];
+let actual5 = mergeIntervals(intervals5);
+console.log(JSON.stringify(actual5) === JSON.stringify(expected5)); // true
