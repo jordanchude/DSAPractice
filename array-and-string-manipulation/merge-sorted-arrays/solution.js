40db3b21f31bfeba2133f7d2244c9c9ba2b6a1af
Jordan <jordan@chude.co>
2023-04-10 22:41:50 -0400

    Refactor mergeArrays function and add test cases in solution.js

--- array-and-string-manipulation/merge-sorted-arrays/solution.js
+++ array-and-string-manipulation/merge-sorted-arrays/solution.js
@@ -18,8 +18,8 @@ function mergeArrays(firstArray, secondArray) {
         } else if (firstArray[i] < secondArray[j]) {
           mergedArray[currentMerge] = firstArray[i]
           i++;
-        } else if (secondArray[j] < firstArray[i]) {
-          mergedArray[currentMerge] = firstArray[i]
+        } else {
+          mergedArray[currentMerge] = secondArray[j]
           j++;
         }
 
@@ -56,37 +56,37 @@ function mergeArrays(firstArray, secondArray) {
   
   // Tests
   
-  // let desc = 'both arrays are empty';
-  // let actual = mergeArrays([], []);
-  // let expected = [];
-  // assertDeepEqual(actual, expected, desc);
+  let desc = 'both arrays are empty';
+  let actual = mergeArrays([], []);
+  let expected = [];
+  assertDeepEqual(actual, expected, desc);
   
-  // desc = 'first array is empty';
-  // actual = mergeArrays([], [1, 2, 3]);
-  // expected = [1, 2, 3];
-  // assertDeepEqual(actual, expected, desc);
+  desc = 'first array is empty';
+  actual = mergeArrays([], [1, 2, 3]);
+  expected = [1, 2, 3];
+  assertDeepEqual(actual, expected, desc);
   
-  // desc = 'second array is empty';
-  // actual = mergeArrays([5, 6, 7], []);
-  // expected = [5, 6, 7];
-  // assertDeepEqual(actual, expected, desc);
+  desc = 'second array is empty';
+  actual = mergeArrays([5, 6, 7], []);
+  expected = [5, 6, 7];
+  assertDeepEqual(actual, expected, desc);
   
-  // desc = 'both arrays have some numbers';
-  // actual = mergeArrays([2, 4, 6], [1, 3, 7]);
-  // expected = [1, 2, 3, 4, 6, 7];
-  // assertDeepEqual(actual, expected, desc);
+  desc = 'both arrays have some numbers';
+  actual = mergeArrays([2, 4, 6], [1, 3, 7]);
+  expected = [1, 2, 3, 4, 6, 7];
+  assertDeepEqual(actual, expected, desc);
   
-  // desc = 'arrays are different lengths';
-  // actual = mergeArrays([2, 4, 6, 8], [1, 7]);
-  // expected = [1, 2, 4, 6, 7, 8];
-  // assertDeepEqual(actual, expected, desc);
+  desc = 'arrays are different lengths';
+  actual = mergeArrays([2, 4, 6, 8], [1, 7]);
+  expected = [1, 2, 4, 6, 7, 8];
+  assertDeepEqual(actual, expected, desc);
   
-  // function assertDeepEqual(a, b, desc) {
-  //   const aStr = JSON.stringify(a);
-  //   const bStr = JSON.stringify(b);
-  //   if (aStr !== bStr) {
-  //     console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
-  //   } else {
-  //     console.log(`${desc} ... PASS`);
-  //   }
-  // }
+  function assertDeepEqual(a, b, desc) {
+    const aStr = JSON.stringify(a);
+    const bStr = JSON.stringify(b);
+    if (aStr !== bStr) {
+      console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
+    } else {
+      console.log(`${desc} ... PASS`);
+    }
+  }
