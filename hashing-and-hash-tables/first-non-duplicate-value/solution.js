a7dc391e5ef5fd7ccf6fac7166e39d1a2f0abebf
Jordan <jordan@chude.co>
2023-04-26 11:00:20 -0400

    "Implement solution to find the first non-duplicate value in a string using a hash table and identifying which duplicate came first, keeping track of index and returning the first non-duplicate value."

--- hashing-and-hash-tables/first-non-duplicate-value/solution.js
+++ hashing-and-hash-tables/first-non-duplicate-value/solution.js
@@ -1,5 +1,22 @@
 function firstNonDuplicateCharacter(str) {
-    // Your implementation here
+    // notes
+        // use hash table
+        // find way to identify which duplicate came first
+        // there will be two values that are non-duplicates
+        // find non-duplicate values
+        // return which one is first in the string
+        // need to keep track of two things
+            // if value is non-duplicate
+            // index
+    // variables
+        // hashTable
+    // pseudocode
+        // loop through string
+            // initialize key, value pair in hashTable with index
+        // loop through hashTable
+            // if 
+    // edge cases
+        // there are two non-duplicate values
 }
 
 // Test cases
